f3b5012538e64c8762169eba473685b8714adf0b
Easin Muttaqi <easin.muttaqi@gmail.com>
2019-07-31 16:25:42 +0600

    Initial Commit

---
+++ basic_app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from django.contrib.auth.models import User
+from .models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+# From
+# https://medium.com/@frfahim/django-registration-with-confirmation-email-bb5da011e4ef
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')
---
+++ learning_user/urls.py
@@ -0,0 +1,31 @@
+"""learning_user URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from basic_app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('accounts/', include('basic_app.urls')),
+    # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     views.activate, name='activate'),
+    path('admin/', admin.site.urls),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
